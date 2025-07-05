aee67e4342352fb12a2e0fefa2b34c35edef7c86
Wangbinhbui <quangbinhbui26038897@gmail.com>
2025-07-06 01:32:19 +0700

    update class AuthServiceImplTest to test register function

--- src/test/java/com/fpt/capstone/tourism/service/impl/AuthServiceImplTest.java
+++ src/test/java/com/fpt/capstone/tourism/service/impl/AuthServiceImplTest.java
@@ -221,5 +221,335 @@ class AuthServiceImplTest {
 
 
 
-  
+
+    @Test
+    void login_emailNotConfirmed_throwsException() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUsername("testuser");
+        userDTO.setPassword("password");
+
+        User user = User.builder().username("testuser").deleted(false).emailConfirmed(false).build();
+
+        when(userService.findUserByUsername("testuser")).thenReturn(user);
+
+        assertThrows(BusinessException.class, () -> authService.login(userDTO));
+    }
+
+    @Test
+    void register_success() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        when(userService.existsByUsername(anyString())).thenReturn(false);
+        when(userService.exitsByEmail(anyString())).thenReturn(false);
+        when(userService.existsByPhoneNumber(anyString())).thenReturn(false);
+        when(roleRepository.findByRoleName(anyString())).thenReturn(Optional.of(Role.builder().roleName("CUSTOMER").build()));
+        when(passwordEncoder.encode(anyString())).thenReturn("encoded");
+        when(userService.saveUser(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(userRoleRepository.save(any(UserRole.class))).thenReturn(null);
+        when(emailConfirmationService.createEmailConfirmationToken(any(User.class))).thenReturn(Token.builder().build());
+
+        GeneralResponse<UserInfoResponseDTO> response = authService.register(req);
+
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+        assertNotNull(response.getData());
+        assertEquals("testuser2", response.getData().getUsername());
+        assertEquals("Cảm ơn bạn đã đăng ký. Vui lòng kiểm tra email để hoàn tất xác minh", response.getMessage());
+    }
+
+    @Test
+    void register_phoneExists_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        when(userService.existsByUsername(anyString())).thenReturn(false);
+        when(userService.exitsByEmail(anyString())).thenReturn(false);
+        when(userService.existsByPhoneNumber(anyString())).thenReturn(true);
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Số điện thoại đã được sử dụng", exception.getMessage());
+    }
+    @Test
+    void register_invalidPhoneFormat_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone("035"); // Invalid phone number
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Số điện thoại phải gồm đúng 10-15 chữ số", exception.getMessage());
+    }
+    @Test
+    void register_phoneWithSpecialChar_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone("035@567890"); // Invalid phone number with special character
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Số điện thoại phải gồm đúng 10-15 chữ số", exception.getMessage());
+    }
+    @Test
+    void register_phonenull_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone(""); // null phone number
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Số điện thoại không được để trống", exception.getMessage());
+    }
+    @Test
+    void register_invalidPasswordFormat_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motcon"); // Invalid password
+        req.setRePassword("motconvit");
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Mật khẩu phải từ 8 ký tự trở lên, bao gồm ít nhất 1 chữ hoa, 1 chữ thường và 1 ký tự đặc biệt", exception.getMessage());
+    }
+    @Test
+    void register_rePasswordMismatch_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit123!"); // Mismatched rePassword
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Mật khẩu và xác nhận mật khẩu không trùng khớp", exception.getMessage());
+    }
+    @Test
+    void register_passwordNull_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword(""); // Null or empty password
+        req.setRePassword("");
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Mật khẩu không được để trống", exception.getMessage());
+    }
+    @Test
+    void register_rePasswordNull_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword(""); // Null or empty rePassword
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Xác nhận mật khẩu không được để trống", exception.getMessage());
+    }
+    @Test
+    void register_invalidFullName_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName(" test ! user"); // Invalid full name
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Họ tên phải bắt đầu bằng chữ cái, chỉ chứa chữ cái và khoảng trắng", exception.getMessage());
+    }
+    @Test
+    void register_fullNameNull_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName(""); // Null or empty full name
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Họ và tên không được để trống", exception.getMessage());
+    }
+    @Test
+    void register_emailExists_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("testuser@gmail.com");
+
+        when(userService.existsByUsername(anyString())).thenReturn(false);
+        when(userService.exitsByEmail("testuser@gmail.com")).thenReturn(true);
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Email đã được sử dụng", exception.getMessage());
+    }
+    @Test
+    void register_invalidEmailFormat_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("testuser @gmail.com"); // Invalid email
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Email không hợp lệ", exception.getMessage());
+    }
+    @Test
+    void register_emailNull_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail(""); // Null or empty email
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Email không được để trống", exception.getMessage());
+    }
+    @Test
+    void register_usernameExists_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        when(userService.existsByUsername("testuser")).thenReturn(true);
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Tên đăng nhập đã tồn tại", exception.getMessage());
+    }
+    @Test
+    void register_fullNameTooLong_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("This is a very long full name that exceeds fifty characters in total length!");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Họ tên phải bắt đầu bằng chữ cái, chỉ chứa chữ cái và khoảng trắng", exception.getMessage());
+    }
+    @Test
+    void register_phoneNotStartWithZero_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("testuser2");
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone("1234567890"); // Does not start with 0
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Đăng ký tài khoản thất bại do lỗi hệ thống!", exception.getMessage());
+    }
+    @Test
+    void register_invalidUsernameTooShort_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("test"); // Too short
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Tên đăng nhập chỉ bao gồm chữ cái, số, dấu gạch ngang (-), gạch dưới (_) và có độ dài từ 8 đến 30 ký tự", exception.getMessage());
+    }
+    @Test
+    void register_usernameTooLong_throwsException() {
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername("abcdefghijklmnopqrstuvwxyz01234567890123456789"); // >36 chars
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        Exception exception = assertThrows(BusinessException.class, () -> authService.register(req));
+        assertEquals("Tên đăng nhập chỉ bao gồm chữ cái, số, dấu gạch ngang (-), gạch dưới (_) và có độ dài từ 8 đến 30 ký tự", exception.getMessage());
+    }
+    @Test
+    void register_usernameExactly30Chars_success() {
+        String username = "abcdefghijklmnopqrstuvwxyz1234"; // 30 chars
+        RegisterRequestDTO req = new RegisterRequestDTO();
+        req.setUsername(username);
+        req.setPassword("Motconvit!");
+        req.setRePassword("Motconvit!");
+        req.setFullName("User Name");
+        req.setPhone("0123456789");
+        req.setAddress("Address");
+        req.setEmail("user@email.com");
+
+        when(userService.existsByUsername(username)).thenReturn(false);
+        when(userService.exitsByEmail(anyString())).thenReturn(false);
+        when(userService.existsByPhoneNumber(anyString())).thenReturn(false);
+        when(roleRepository.findByRoleName(anyString())).thenReturn(Optional.of(Role.builder().roleName("CUSTOMER").build()));
+        when(passwordEncoder.encode(anyString())).thenReturn("encoded");
+        when(userService.saveUser(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(userRoleRepository.save(any(UserRole.class))).thenReturn(null);
+        when(emailConfirmationService.createEmailConfirmationToken(any(User.class))).thenReturn(Token.builder().build());
+
+        GeneralResponse<UserInfoResponseDTO> response = authService.register(req);
+
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+        assertNotNull(response.getData());
+        assertEquals(username, response.getData().getUsername());
+        assertEquals("Cảm ơn bạn đã đăng ký. Vui lòng kiểm tra email để hoàn tất xác minh", response.getMessage());
+    }
 }
