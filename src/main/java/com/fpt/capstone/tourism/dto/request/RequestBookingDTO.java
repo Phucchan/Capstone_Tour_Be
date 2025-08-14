e64aa7cb15d33647dd3161d8478741f96bc227da
Wangbinhbui <quangbinhbui26038897@gmail.com>
2025-08-14 17:03:14 +0700

    create unit test

--- src/main/java/com/fpt/capstone/tourism/dto/request/RequestBookingDTO.java
+++ src/main/java/com/fpt/capstone/tourism/dto/request/RequestBookingDTO.java
@@ -4,6 +4,7 @@ import com.fpt.capstone.tourism.model.enums.RequestBookingStatus;
 import com.fpt.capstone.tourism.model.enums.RoomCategory;
 import com.fpt.capstone.tourism.model.enums.TourTransport;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder(toBuilder = true)
 public class RequestBookingDTO {
     private Long id;
     private Long userId;
