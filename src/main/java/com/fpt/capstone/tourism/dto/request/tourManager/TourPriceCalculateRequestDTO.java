9fde59a5fed9feec523c7d7193174d746d11962c
Mynameiskiet <104210040+Mynameiskiet@users.noreply.github.com>
2025-08-27 02:39:41 +0700

    u calculate price

--- src/main/java/com/fpt/capstone/tourism/dto/request/tourManager/TourPriceCalculateRequestDTO.java
+++ src/main/java/com/fpt/capstone/tourism/dto/request/tourManager/TourPriceCalculateRequestDTO.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TourPriceCalculateRequestDTO {
-    private Double extraCost;
+
     private Double profitRate;
 }
