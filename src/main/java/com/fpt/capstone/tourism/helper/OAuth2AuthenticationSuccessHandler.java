542f4f6ca5b275eeca0d18fe72aaa8612ee6b974
Hoai Nam <nvhnam.work@gmail.com>
2025-08-28 10:29:51 +0700

    u

--- src/main/java/com/fpt/capstone/tourism/helper/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/fpt/capstone/tourism/helper/OAuth2AuthenticationSuccessHandler.java
@@ -1,9 +1,15 @@
 package com.fpt.capstone.tourism.helper;
 
 import com.fpt.capstone.tourism.helper.IHelper.JwtHelper;
+import com.fpt.capstone.tourism.model.Role;
 import com.fpt.capstone.tourism.model.User;
+import com.fpt.capstone.tourism.model.UserRole;
+import com.fpt.capstone.tourism.model.enums.Provider;
 import com.fpt.capstone.tourism.model.enums.RoleName;
+import com.fpt.capstone.tourism.model.oauth2.OAuthAccount;
+import com.fpt.capstone.tourism.repository.oauth2.OAuthAccountRepository;
 import com.fpt.capstone.tourism.repository.user.UserRepository;
+import com.fpt.capstone.tourism.repository.user.UserRoleRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +28,8 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
 
     private final JwtHelper jwtHelper;
     private final UserRepository userRepository;
+    private final UserRoleRepository userRoleRepository;
+    private final OAuthAccountRepository oAuthAccountRepository;
 
     @Value("${frontend.base-url}")
     private String frontendBaseUrl;
@@ -34,10 +42,44 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
         // Lấy thông tin email từ OAuth2AuthenticationToken
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+
+        User user = userRepository.findByEmail(email)
+                .orElseGet(() -> {
+                    User newUser = new User();
+                    newUser.setEmail(email);
+                    newUser.setFullName(name);
+                    newUser.setUsername(email);
+                    newUser.setPassword("Motconvit!"); // No password for OAuth
+                    User savedUser =  userRepository.save(newUser);
+
+                    UserRole userRole = UserRole.builder()
+                            .user(savedUser)
+                            .role(Role.builder().id(1L).build())
+                            .deleted(false)
+                            .build();
+
+                    userRoleRepository.save(userRole);
+
+                    return savedUser;
+                });
+
+
+        // 5. Find or create OAuthAccount
+        oAuthAccountRepository.findByProviderAndUserId(Provider.GOOGLE, user.getId())
+                .orElseGet(() -> {
+                    OAuthAccount account = new OAuthAccount();
+                    account.setUser(user);
+                    account.setProvider(Provider.GOOGLE);
+                    account.setProviderId(oAuth2User.getAttribute("sub")); // Google dùng "sub"
+                    return oAuthAccountRepository.save(account);
+                });
 
         // Tạo token
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found: " + email));
+//        User user = userRepository.findByEmail(email)
+//                .orElseThrow(() -> new RuntimeException("User not found: " + email));
+
+
 
         // Tạo JWT token
         String token = jwtHelper.generateToken(user);
